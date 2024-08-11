113e22380f8ce2b8d0df1f14e5968a1755e35744
Roman Kulish <roman.kulish@gmail.com>
2024-08-11 19:20:40 +1000

    chore: Refactor VEML6030 code to use std::min and std::max
    Refactor the VEML6030 code to use the std::min and std::max functions instead of the macros. This improves code readability and avoids potential conflicts with other libraries or code that may define the min and max macros.

--- lib/VEML6030/VEML6030.cpp
+++ lib/VEML6030/VEML6030.cpp
@@ -1,5 +1,10 @@
 #include "VEML6030.h"
 
+#undef min 
+#undef max
+
+#include <algorithm>
+
 namespace Sensor
 {
     bool VEML6030::begin(TwoWire &wire)
@@ -33,12 +38,12 @@ namespace Sensor
     uint8_t VEML6030::getBrightness()
     {
         float lux = readLux();
-        lux = max(config.minLux, min(lux, config.maxLux));
+        lux = std::max(config.minLux, std::min(lux, config.maxLux));
 
         float ratio = (lux - config.minLux) / (config.maxLux - config.minLux);
         uint8_t led_brightness = static_cast<uint8_t>(ratio * (config.maxBrightness - config.minBrightness)) + config.minBrightness;
 
-        return max(config.minBrightness, min(led_brightness, config.maxBrightness));
+        return std::max(config.minBrightness, std::min(led_brightness, config.maxBrightness));
     }
 
     bool VEML6030::isNightMode()
--- lib/VEML6030/VEML6030.h
+++ lib/VEML6030/VEML6030.h
@@ -2,6 +2,7 @@
 #define VEML6030_H
 
 #include <Arduino.h>
+
 #include <Wire.h>
 #include "SparkFun_VEML6030_Ambient_Light_Sensor.h"
 
@@ -13,43 +14,102 @@ namespace Sensor
     // Configuration struct for VEML6030
     struct VEML6030Config
     {
-        uint8_t address;
-        float gain;
-        uint16_t integrationTime;
-        uint8_t protect;
-        bool powerSaveEnabled;
-        uint16_t powerSaveMode;
-        float minLux;
-        float maxLux;
-        uint8_t minBrightness;
-        uint8_t maxBrightness;
-        uint8_t nightModeThreshold;
-        float filterWeight = 0.1f;
+        uint8_t address = 0x10;          // Default I2C address
+        float gain = 1.0f;               // Default gain (1/8)
+        uint16_t integrationTime = 100;  // Default integration time (100ms)
+        uint8_t protect = 1;             // Default interrupt/alert protection
+        bool powerSaveEnabled = true;    // Default power-saving mode on
+        uint16_t powerSaveMode = 4;      // Default power-saving mode (mode 4)
+        float minLux = 0.0f;             // Default minimum lux level
+        float maxLux = 500.0f;           // Default maximum lux level
+        uint8_t minBrightness = 5;       // Default minimum LED brightness
+        uint8_t maxBrightness = 255;     // Default maximum LED brightness
+        uint8_t nightModeThreshold = 10; // Default night mode threshold
+        float filterWeight = 0.1f;       // Default filter weight for low-pass filter
+    };
 
-        VEML6030Config(
-            uint8_t address = 0x10,          // Default I2C address
-            float gain = 1.0f,               // Default gain (1/8)
-            uint16_t integrationTime = 100,  // Default integration time (100ms)
-            uint8_t protect = 1,             // Default interrupt/alert protection
-            bool powerSaveEnabled = true,    // Default power-saving mode on
-            uint16_t powerSaveMode = 4,      // Default power-saving mode (mode 4)
-            float minLux = 0.0f,             // Default minimum lux level
-            float maxLux = 500.0f,           // Default maximum lux level
-            uint8_t minBrightness = 5,       // Default minimum LED brightness
-            uint8_t maxBrightness = 255,     // Default maximum LED brightness
-            uint8_t nightModeThreshold = 10, // Default night mode threshold
-            float filterWeight = 0.1f) : address(address),
-                                         gain(gain),
-                                         integrationTime(integrationTime),
-                                         protect(protect),
-                                         powerSaveEnabled(powerSaveEnabled),
-                                         powerSaveMode(powerSaveMode),
-                                         minLux(minLux),
-                                         maxLux(maxLux),
-                                         minBrightness(minBrightness),
-                                         maxBrightness(maxBrightness),
-                                         nightModeThreshold(nightModeThreshold),
-                                         filterWeight(filterWeight) {};
+    class VEML6030ConfigBuilder
+    {
+    public:
+        VEML6030ConfigBuilder &setAddress(uint8_t address)
+        {
+            config.address = address;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setGain(float gain)
+        {
+            config.gain = gain;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setIntegrationTime(uint16_t integrationTime)
+        {
+            config.integrationTime = integrationTime;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setProtect(uint8_t protect)
+        {
+            config.protect = protect;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setPowerSaveEnabled(bool powerSaveEnabled)
+        {
+            config.powerSaveEnabled = powerSaveEnabled;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setPowerSaveMode(uint16_t powerSaveMode)
+        {
+            config.powerSaveMode = powerSaveMode;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setMinLux(float minLux)
+        {
+            config.minLux = minLux;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setMaxLux(float maxLux)
+        {
+            config.maxLux = maxLux;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setMinBrightness(uint8_t minBrightness)
+        {
+            config.minBrightness = minBrightness;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setMaxBrightness(uint8_t maxBrightness)
+        {
+            config.maxBrightness = maxBrightness;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setNightModeThreshold(uint8_t nightModeThreshold)
+        {
+            config.nightModeThreshold = nightModeThreshold;
+            return *this;
+        }
+
+        VEML6030ConfigBuilder &setFilterWeight(float filterWeight)
+        {
+            config.filterWeight = filterWeight;
+            return *this;
+        }
+
+        VEML6030Config build()
+        {
+            return config;
+        }
+
+    private:
+        VEML6030Config config;
     };
 
     class VEML6030
@@ -69,4 +129,5 @@ namespace Sensor
         float filteredLux = 0.0f;
     };
 }
+
 #endif
